c86e0af7a1e1a222433dea44a9cbbd6ceb28cf21
Ajinkya Chhatre <chhatrea@vmware.com>
2017-06-19 23:20:22 +0530

    SASS support
    Initial check-in.

---
+++ src/app/ticker/ticker.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-ticker',
+    templateUrl: './ticker.component.html',
+    styleUrls: ['./ticker.component.scss']
+})
+export class TickerComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+}
