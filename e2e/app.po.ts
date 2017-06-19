eb625921991a1b2da2c27d2f04f08d219582ff70
Ajinkya Chhatre <chhatrea@vmware.com>
2017-06-20 01:07:48 +0530

    e2e page object

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,15 @@
 import { browser, element, by } from 'protractor';
 
 export class RandomNumberTickerPage {
-  navigateTo() {
-    return browser.get('/');
-  }
+    navigateTo() {
+        return browser.get('/');
+    }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
+    getParagraphText() {
+        return element(by.css('app-root p')).getText();
+    }
+
+    getElementById(id: string) {
+        return element(by.id(id));
+    }
 }
