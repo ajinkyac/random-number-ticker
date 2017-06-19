c5dce7cfd99a0b2cf8c54beabacd828010f1c8ff
Ajinkya Chhatre <chhatrea@vmware.com>
2017-06-20 00:37:25 +0530

    Service updated
    TimerObservable not useful for this problem.

--- src/app/service/random-number/random-number.service.ts
+++ src/app/service/random-number/random-number.service.ts
@@ -7,7 +7,7 @@ export class RandomNumberService {
     private static readonly RANDOM_FN_MIN = 1;
     private static readonly RANDOM_FN_MAX = 15000;
 
-    public uniqueRandomNumberSubject: Subject<any> = new Subject<any>();
+    public uniqueRandomNumberSubject: Subject<number[]> = new Subject<number[]>();
     public sequentialNumberSubject: Subject<number> = new Subject<number>();
 
     constructor() { }
@@ -16,7 +16,14 @@ export class RandomNumberService {
 
     }
 
+    /**
+     * NOTE: A demo function left as a food for thought if the TimerObservable could be used.
+     */
     publishEmulatedRandomness(): void {
+        /*
+        // This also works well if the numbers are stored, shuffled and displayed to emulate randomness.
+        // But TimerObservable is a costly affair w.r.t it's disposal.
+
         const timer = TimerObservable.create(0, 2000);
 
         timer.subscribe((number) => {
@@ -27,5 +34,7 @@ export class RandomNumberService {
                 this.sequentialNumberSubject.next(null);
             }
         });
+
+        */
     }
 }
